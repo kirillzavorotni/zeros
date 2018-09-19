edddd1ae072b96b5d76bf4481b588a7942197725
Kiryl Zavarotny <kirillzavorotni@gmail.com>
2018-09-19 23:28:06 +0300

    add function getZerosCount for calculate zeros in the end a factorial of number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var counter = 0;
+    var i = 5;
+    do {
+        var result = Math.floor(number / i);
+        counter += result;
+        i *= 5;
+    } while(result > 1);
+    return counter;
 }
